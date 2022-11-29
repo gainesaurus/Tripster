35365b65127eff7f0bcffaf18b74817ffda98290
Danielle Stroscher <daniellejs98@gmail.com>
2022-11-29 07:38:10 -0800

    feat: added google link to pin locations

--- src/components/PinDropItem/PinDropItem.tsx
+++ src/components/PinDropItem/PinDropItem.tsx
@@ -13,36 +13,48 @@ const PinDropItem: FC<PinDropItemProps> =({ location }) => {
   const latLng = location.latLng.split(',');
 
   const mapStyle = {
-    width: '200px',
-    height: '400px',
+    width: '100%',
+    height: '100%',
   };
   const center = {
     lat: Number(latLng[0]),
     lng: Number(latLng[1])
   };
+  const mapOptions = {
+    fullscreenControl: false,
+    keyboardShortcuts: false,
+  };
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-  })
+  });
 
   return (
+    <>
     <div className={styles.pinDropContainer}>
+      {
+        isLoaded?
+        <GoogleMap
+        mapContainerStyle={mapStyle}
+        center={center}
+        zoom={13}
+        options={mapOptions}
+        >
+          <Marker
+          position={center}
+          />
+        </GoogleMap> : <></>
+      }
 
-        {
-          isLoaded?
-          <GoogleMap
-          mapContainerStyle={mapStyle}
-          center={center}
-          zoom={10}
-          >
-            <Marker position={center} />
-          </GoogleMap> : <></>
-        }
-        <div className={styles.pinDropInfo}>{location.info}</div>
-
+      <div className={styles.pinDropInfo}>
+        {location.info + ' '}
+        <a
+        className={styles.mapLink}
+        href={`https://www.google.com/maps/search/?api=1&query=${latLng[0]}%2C${latLng[1]}`}>Get directions</a>
+      </div>
     </div>
+    </>
   )
 }
-<script src="https://maps.googleapis.com/maps/api/js?key=[YOUR_API_KEY]&callback=initialize" async defer></script>
 
 export default PinDropItem;
