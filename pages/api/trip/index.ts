9982a667d22c721cc2115514baa5d147b04fd074
Michael Gainey <gainey.michael@gmail.com>
2022-11-29 15:00:08 -0800

    Revert "Add logo"

--- pages/api/trip/index.ts
+++
@@ -1,7 +0,0 @@
-import nc from 'next-connect';
-import { getAllTrips, addNewTrip } from '../../../controllers/trip';
-import verifyToken from '../../../middleware/verifyToken';
-
-const handler = nc().use(verifyToken).get(getAllTrips).post(addNewTrip);
-
-export default handler;
--- src/components/Divider/Divider.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Divider.module.css';
-
-function Divider() {
-  return <div className={styles.divide}></div>;
-}
-
-export default Divider;
