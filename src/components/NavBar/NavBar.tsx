7705424adef2a236da2fdb9a6287b13e966533ea
gainey.michael <gainey.michael@gmail.com>
2022-11-29 15:51:30 -0800

    logo update and media query for nav greeting

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -23,7 +23,7 @@ const NavBar = () => {
   return (
     <div className={styles.navigation}>
       <img className={styles.logo} onClick={goToHome}
-        src='https://static.wixstatic.com/media/3dbed1_672e6e2a9fad4b57b166897e41d8ac31~mv2.png/v1/fill/w_418,h_163,al_c,q_85,enc_auto/3dbed1_672e6e2a9fad4b57b166897e41d8ac31~mv2.png'
+        src='https://static.wixstatic.com/media/3dbed1_550db7f924204c39b795fb469389b157~mv2.png/v1/fill/w_425,h_149,al_c,q_85,enc_auto/3dbed1_550db7f924204c39b795fb469389b157~mv2.png'
         alt='vakay logo'
       />
       {router.pathname !== '/login' ? (
