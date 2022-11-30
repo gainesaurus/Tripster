b0a13a1eeeaaf61061beedef47bc647e89c36218
gainey.michael <gainey.michael@gmail.com>
2022-11-30 13:17:18 -0800

    feat: add event dropdown

--- src/services/eventService.tsx
+++ src/services/eventService.tsx
@@ -1,6 +1,7 @@
 const BASEurl = '';
+import { IEvent } from "../../Types";
 
-export const getEventsByTripId = async (tripId:string) => {
+export const getEventsByTripId = async (tripId:number) => {
   try {
     const result = await fetch(`${BASEurl}/events/${tripId}`, {
       method: 'GET',
@@ -12,9 +13,26 @@ export const getEventsByTripId = async (tripId:string) => {
   }
 }
 
-export const updateEvents = async (tripId:string, event:any) => {
+export const createEvent = async (tripId:number, event:IEvent) => {
   try {
     const result = await fetch(`${BASEurl}/events/${tripId}`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(event)
+    });
+    return await result.json();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const updateEvent = async (tripId:number, _id:number, event:IEvent) => {
+  try {
+    const result = await fetch(`${BASEurl}/events/${tripId}/${_id}`, {
       method: 'PUT',
       mode: 'cors',
       headers: {
