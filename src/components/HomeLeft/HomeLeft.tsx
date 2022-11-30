1b5dc5faca31c52efd5d19776a502f6fadf9d672
Danielle Stroscher <daniellejs98@gmail.com>
2022-11-29 16:41:12 -0800

    added current trip list

--- src/components/HomeLeft/HomeLeft.tsx
+++ src/components/HomeLeft/HomeLeft.tsx
@@ -5,13 +5,15 @@ import { ITripItem } from '../../../Types';
 
 type HomeLeftProps = {
   currentTrips: ITripItem[];
+  upcomingTrips: ITripItem[];
   pastTrips: ITripItem[];
 };
 
-function HomeLeft({ currentTrips, pastTrips }: HomeLeftProps) {
+function HomeLeft({ currentTrips, upcomingTrips, pastTrips }: HomeLeftProps) {
   return (
     <div className={styles.homeLeft}>
-      <TripList trips={currentTrips} title={'Upcoming Trips'} />
+      <TripList trips={currentTrips} title={'Current Trips'} />
+      <TripList trips={upcomingTrips} title={'Upcoming Trips'} />
       <TripList trips={pastTrips} title={'Memories'} />
     </div>
   );
