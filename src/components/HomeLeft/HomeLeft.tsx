78c935ca0a8179d77f6caebfff0ae79a0130b156
Gabriel Almeida Rizzutto <garizzutto@gmail.com>
2022-12-01 18:14:54 -0300

    fix: merge fix

--- src/components/HomeLeft/HomeLeft.tsx
+++ src/components/HomeLeft/HomeLeft.tsx
@@ -1,7 +1,6 @@
+import { AddBox } from '@mui/icons-material';
 import { useTripsContext } from '../../Contexts/TripsContext';
 import TripList from '../TripList/TripList';
-import { ITripItem } from '../../../Types';
-import { AddBox } from '@mui/icons-material';
 
 import styles from './HomeLeft.module.css';
 
@@ -11,27 +10,34 @@ type HomeLeftProps = {
 
 function HomeLeft({ openForm }: HomeLeftProps) {
   const context = useTripsContext();
-  
+
   return (
     <div className={styles.homeLeft}>
       <div className={styles.tripsTitleBox}>
         <h2 className={styles.tripsTitle}>My Trips:</h2>
-        <button className={styles.addTripBtn} title="Add Trip" onClick={openForm}>
-          <AddBox className={styles.addIcon}/>
+        <button
+          className={styles.addTripBtn}
+          title="Add Trip"
+          onClick={openForm}
+        >
+          <AddBox className={styles.addIcon} />
         </button>
       </div>
-      {
-        currentTrips && currentTrips.length > 0 ?
-        <TripList trips={context.currentTrips} title={'Current Trips'} /> : <></>
-      }
-      {
-        upcomingTrips && upcomingTrips.length > 0 ?
-        <TripList trips={context.upcomingTrips} title={'Upcoming Trips'} /> : <></>
-      }
-      {
-        pastTrips && pastTrips.length > 0 ?
-        <TripList trips={context.pastTrips} title={'Memories'} /> : <></>
-      }
+      {context.currentTrips.length > 0 ? (
+        <TripList trips={context.currentTrips} title={'Current Trips'} />
+      ) : (
+        <></>
+      )}
+      {context.upcomingTrips.length > 0 ? (
+        <TripList trips={context.upcomingTrips} title={'Upcoming Trips'} />
+      ) : (
+        <></>
+      )}
+      {context.pastTrips.length > 0 ? (
+        <TripList trips={context.pastTrips} title={'Memories'} />
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
