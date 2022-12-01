f00f374c97540dd3dfc4adccf953afef88775527
gainey.michael <gainey.michael@gmail.com>
2022-12-01 07:21:50 -0800

    feat: fixed backend for events

--- controllers/events.ts
+++ controllers/events.ts
@@ -1,13 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Events from '../models/Events';
-import { IEvent } from '../Types';
+import Trip from '../models/Trip';
+import { IEvent, ITripItem } from '../Types';
 
 export async function getAllEvents(
   req: NextApiRequest,
   res: NextApiResponse<IEvent[]>,
 ) {
   const events = await Events.find<IEvent>({
-    attendees: req.body.uid,
+    tripId: req.body.tripId,
   }).exec();
   res.status(200).json(events);
 }
@@ -17,19 +18,23 @@ export async function addNewEvent(
   res: NextApiResponse<IEvent | { error: unknown }>,
 ) {
   try {
-    if (!req.body.attendees) req.body.attendees = [];
-    if (!req.body.attendees.includes(req.body.uid))
-      req.body.attendees.push(req.body.uid);
-    const event = new Events({
-      tripId: req.body.tripId,
-      title: req.body.title,
-      startDate: req.body.startDate,
-      endDate: req.body.endDate,
-      eventType: req.body.eventType,
-      info: req.body.info,
-    });
-    await event.save();
-    res.status(200).json(event);
+    const trip = await Trip.findOne<ITripItem>({
+      $and: [{ _id: req.body.tripId }, { attendees: req.body.uid }],
+    }).exec();
+    if(trip) {
+      const event = new Events({
+        tripId: req.body.tripId,
+        title: req.body.title,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        eventType: req.body.eventType,
+        info: req.body.info,
+      });
+      await event.save();
+      trip!.events?.push(event._id);
+      await Trip.findByIdAndUpdate(req.body.tripId, trip!).exec();
+      res.status(200).json(event);
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ error });
