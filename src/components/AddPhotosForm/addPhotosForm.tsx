b2b38f83c7bb0fbf12de683ad3d4f0c3a20230a9
Danielle Stroscher <daniellejs98@gmail.com>
2022-12-04 14:11:07 -0800

    feat: maintined photo quality during upload

--- src/components/AddPhotosForm/addPhotosForm.tsx
+++ src/components/AddPhotosForm/addPhotosForm.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { IPhoto } from '../../../Types';
 import styles from './addPhotosForm.module.css';
 import { Close } from '@mui/icons-material';
+import { MoonLoader } from 'react-spinners';
+import imageSize from '@coderosh/image-size';
 
 import { FilePond, registerPlugin } from 'react-filepond';
 import { FilePondFile } from 'filepond';
@@ -13,6 +15,7 @@ import { uploadPhotosToAlbum } from '../../firebase';
 import { useUserContext } from '../../Contexts/UserContext';
 import { addPhotos } from '../../services/photoService';
 import { useRouter } from 'next/router';
+import { getImageSize } from 'next/dist/server/image-optimizer';
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
@@ -26,55 +29,63 @@ function AddPhotosForm ({ closeForm, setPhotos, allPhotos }:AddPhotosProps) {
 
   const user = useUserContext()
   const [imgFiles, setImgFiles] = React.useState<FilePondFile[]>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   const router = useRouter()
   const tripId = router.query.id;
 
-  // const uploadPic = async () => {
-  //   const token = user.authUser?.token;
-  //   for (const filepond of imgFiles) {
-  //     const blob = await filepond.file.arrayBuffer();
-  //     let res = await uploadPhotosToAlbum(blob);
-  //     let photo = {
-  //       src:res?.imageUrl,
-  //       height:13,
-  //       width:20,
-  //       tripId: tripId,
-  //     }
+  const uploadPic = async () => {
+    const token = user.authUser?.token;
+    for (const filepond of imgFiles) {
+      const blob = await filepond.file.arrayBuffer();
 
-  //     if(photo) {
-  //       console.log(photo);
-  //       photo && addPhotos(token as string, photo as IPhoto).then((photo)=>{setPhotos({...allPhotos, photo})});
-  //     } else {
-  //       console.log('could not save photo.')
-  //     }
-  //   }
-  //   return;
-  // };
+      const size = await imageSize(blob)
+      console.log(size)
 
-  // async function handleSubmit(e:any) {
-  //   e.preventDefault();
-  //   const token = user.authUser?.token;
-  //   if (token && imgFiles.length > 0) {
-  //     console.log('about to upload...');
-  //     await uploadPic();
-  //     console.log('upload done');
-  //     setImgFiles([]);
-  //   } else {
-  //     alert('All fields are required');
-  //   }
-  // }
-  //console.log(imgFiles);
+      let res = await uploadPhotosToAlbum(blob);
+      console.log(blob, 'blob')
+      let photo = {
+        src:res?.imageUrl,
+        height:size.height,
+        width:size.width,
+        tripId: tripId,
+      }
+      if(photo) {
+        console.log(photo);
+        let returnedPic;
+        returnedPic = await addPhotos(token as string, photo as IPhoto)
+        setPhotos({...allPhotos, returnedPic});
+      } else {
+        console.log('could not save photo.')
+      }
+    }
+    return;
+  };
+
+  async function handleSubmit(e:any) {
+    e.preventDefault();
+    const token = user.authUser?.token;
+    if (token && imgFiles.length > 0) {
+      setIsLoading(true);
+      await uploadPic();
+      setIsLoading(false);
+      setImgFiles([]);
+      closeForm();
+    } else {
+      alert('All fields are required');
+    }
+  }
+  console.log(imgFiles);
 
   return(
     <div className={styles.addAttendeeContainer}>
       <button className={styles.XButton} onClick={closeForm}>
         <Close />
       </button>
-      <form className={styles.infoContainer} >
+      <form className={styles.infoContainer} onSubmit={handleSubmit}>
         <h2>Share your photo's!</h2>
         <FilePond
-          // files={imgFiles.map((fileItem) => fileItem.file)}
+          files={imgFiles.map((fileItem) => fileItem.file)}
           onupdatefiles={setImgFiles}
           allowMultiple={true}
           acceptedFileTypes={['image/*']}
@@ -82,14 +93,22 @@ function AddPhotosForm ({ closeForm, setPhotos, allPhotos }:AddPhotosProps) {
           // name="files" /* sets the file input name, it's filepond by default */
           // labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
         />
-        <div className={styles.buttonDiv}>
-          <button
-            type='submit'
-            className={styles.submitButton}
-          >
-            Submit
-          </button>
-        </div>
+        {
+          isLoading? (
+          <div className={styles.spinnerContainer}>
+            <MoonLoader color="#fff" size={14} speedMultiplier={0.75} />
+          </div>
+          ) : (
+          <div className={styles.buttonDiv}>
+            <button
+              type='submit'
+              className={styles.submitButton}
+            >
+              Submit
+            </button>
+          </div>
+          )
+        }
       </form>
     </div>
   )
--- src/components/PhotoAlbumList/AlbumList.tsx
+++ src/components/PhotoAlbumList/AlbumList.tsx
@@ -17,15 +17,18 @@ interface AlbumListProps {
 const  AlbumList = ({ tripId }:AlbumListProps) => {
   const [allPhotos, setPhotos] = useState<IPhoto[]>([]);
   const user = useUserContext();
+
   useEffect(() => {
     getTripPhotos()
-  })
+  }, [setPhotos]);
+
   const getTripPhotos = async () => {
     const photos = await getPhotosByTripId(user.authUser!.token, tripId as string)
     if (photos) {
       setPhotos(photos);
     }
   }
+
 
   const openForm = () => {
     document.getElementById('addPhotosForm')!.style.display = 'flex';
@@ -34,6 +37,10 @@ const  AlbumList = ({ tripId }:AlbumListProps) => {
   const closeForm = () => {
     document.getElementById('addPhotosForm')!.style.display = 'none';
   }
+
+  let displayPhotos;
+  allPhotos.length > 10 ? displayPhotos = allPhotos.slice(0, 8)
+  : displayPhotos = allPhotos;
 
   return (
     <div className={styles.albumContainer}>
@@ -47,15 +54,23 @@ const  AlbumList = ({ tripId }:AlbumListProps) => {
         <AddPhotosForm closeForm={closeForm} setPhotos={setPhotos} allPhotos={allPhotos}/>
       </div>
       <div className={styles.albumList}>
-        {allPhotos.length? allPhotos.map((photo: IPhoto, i) => {
-          return <Image key={i + 1} src={photo.src} width={photo.width} height={photo.height} alt='shared photo' className={styles.image}/>
-        }) : <></>}
-        <Link href='[id]/photo-album' as={`${tripId}/photo-album`}>
+        {
+          displayPhotos.length?
+          displayPhotos.map((photo: IPhoto, i) => {
+          return <Image key={i + 1} src={photo.src} width={photo.width} height={photo.height} alt='shared photo' loading={'lazy'} className={styles.image}/>
+          })
+          : <></>
+        }
+        {
+          displayPhotos.length ?
+          <Link href='[id]/photo-album' as={`${tripId}/photo-album`}>
           <div className={styles.viewAll}>
             View all
             <GridViewIcon></GridViewIcon>
           </div>
-        </Link>
+          </Link>
+          : <></>
+        }
       </div>
     </div>
   )
