d5b3d6907cbd07cc7f12d8de38fed558e9be6d8f
Danielle Stroscher <daniellejs98@gmail.com>
2022-12-05 15:13:03 -0800

    jest configured

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,19 +1,18 @@
 import NavBar from './NavBar';
 import { render, screen } from "@testing-library/react";
 import { expect } from '@jest/globals';
-import { BrowserRouter } from 'react-router-dom';
 
 describe('NavBar component', () => {
   test('it should render a logout button when logged in', () => {
     render (
       <NavBar/>
     )
-    expect(screen.getByText('Logout')).toBeInTheDocument();
+    expect(screen.getByText('Logout')).toBeDefined();
   })
-  test('it should render the VaKay logo', () => {
-    //test
-  })
-  test('it should display the user profile pic or default profile logo', () => {
-    //test
-  })
+  // test('it should render the VaKay logo', () => {
+  //   render (
+  //     <NavBar />
+  //   )
+  //})
+
 })
