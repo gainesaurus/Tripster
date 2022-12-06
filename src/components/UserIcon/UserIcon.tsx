9fc3c5b872367025ab5b17d7fa6ea17f3c059013
Danielle Stroscher <daniellejs98@gmail.com>
2022-12-06 08:38:51 -0800

    profile photos have good default display

--- src/components/UserIcon/UserIcon.tsx
+++ src/components/UserIcon/UserIcon.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import styles from './UserIcon.module.css';
 import { IUser } from '../../../Types';
 import Image from 'next/image'
+import { AccountCircle } from '@mui/icons-material';
 
 interface UserProps {
   person: IUser;
@@ -9,12 +10,30 @@ interface UserProps {
 
 const TripItem: FC<UserProps> = ({ person }) => {
 
+  const emailStart = 'daniellestroscher';
+  //person.email.split(/(\S[^\.]*)(\.(\S*))?@(\S*)/ig);
+  console.log(emailStart);
+
   return (
-    <div
-    className={styles.profilePicCont}
-    >
-      <Image className={styles.profilePic} alt={'profile pic'} src={`${person.profile_pic}`} height={500} width={500} />
-    </div>
+    <>
+      {
+        person.profile_pic !== '/add_photo.png'?
+        <div
+        className={styles.profilePicCont}
+        >
+          <Image className={styles.profilePic} alt={'profile pic'} src={`${person.profile_pic}`} height={500} width={500} />
+        </div>
+        :
+        <div className={styles.attendeeBox}>
+          <div
+          className={styles.userIconCont}
+          >
+            <AccountCircle className={styles.blankProfile} fontSize='inherit'/>
+          </div>
+          <p className={styles.name}>{emailStart}</p>
+        </div>
+      }
+    </>
   )
 }
 
