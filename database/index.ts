340ca8c9e78219d5cd195d24bf3d3754c5f25732
Danielle Stroscher <daniellejs98@gmail.com>
2023-01-19 08:19:12 -0800

    fix: db connection string

--- database/index.ts
+++ database/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const uri =
   process.env.CONNECTION_DB !== undefined
     ? process.env.CONNECTION_DB
-    : 'mongodb://127.0.0.1:27017/tripsterdatabase';
+    : 'mongodb://127.0.0.1:27017/vacay-db';
 
 mongoose.connect(uri).catch((error) => console.log(error));
 
